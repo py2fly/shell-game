87bf1cda9e5438fc5e66d540ee408ef595f1c2f5
Vino.E <65939388+py2fly@users.noreply.github.com>
2023-01-10 23:42:54 -0800

    made a correction to index.html added shell2 and shell3 containers

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 /* Imports */
+const shell1Button = document.getElementById('shell1-button');
+const shell2Button = document.getElementById('shell2-button');
+const shell3Button = document.getElementById('shell3-button');
+
+const shedContainer = document.getElementById('shed-container');
+const treeContainer = document.getElementById('tree-container');
+const boulderContainer = document.getElementById('boulder-container');
+
+const totalEl = document.getElementById('total');
+const lossesEl = document.getElementById('losses');
+const winsEl = document.getElementById('wins');
+
+let correctGuesses = 0;
+let totalGuesses = 0;
 
 /* Get DOM Elements */
 
